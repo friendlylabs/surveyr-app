7473ab9014cd898652d80aa3003cc75c4dfc1cf8
ibnsultan <abdulbasitsultan4@gmail.com>
2025-07-20 01:05:19 +0300

    fix: reorder import statement for Ionicons in PanelQuestion component

--- components/survey/PanelQuestion.tsx
+++ components/survey/PanelQuestion.tsx
@@ -1,6 +1,6 @@
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { ParsedQuestion } from '../../utils/surveyParser';
 
 interface PanelQuestionProps {
